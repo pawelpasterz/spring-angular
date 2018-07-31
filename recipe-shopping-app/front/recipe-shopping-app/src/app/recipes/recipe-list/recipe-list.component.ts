d4a5dcfb3b59b15d593d3e78caa369313b693471
ppasterz <ppasterz@microstrategy.com>
2018-07-31 10:17:28 +0200

    Add images

--- recipe-shopping-app/front/recipe-shopping-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-shopping-app/front/recipe-shopping-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../model/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  constructor() { }
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test recipe1', 'Just test bro',
+      'assets/default_recipe.jpg'
+    ),
+    new Recipe(
+      'Test recipe2', 'Just test bro and bla bla bla',
+      'assets/default_recipe.jpg'
+    )
+  ];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
