d9889b04ff2dc8709594150cf239e2da392dbe13
ppasterz <ppasterz@microstrategy.com>
2018-08-13 15:54:37 +0200

    Add initial backend commit

---
+++ recipe-shopping-app/backend/src/main/java/pl/pasterz/recipeshoppinglist/RecipeShoppingListApplication.java
@@ -0,0 +1,12 @@
+package pl.pasterz.recipeshoppinglist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeShoppingListApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RecipeShoppingListApplication.class, args);
+  }
+}
